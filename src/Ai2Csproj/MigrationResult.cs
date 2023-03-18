e60be73e0c2f2083585e74b993cfea8c205ed2ed
Seth Hendrick <seth@shendrick.net>
2023-03-18 17:06:11 -0400

    Added result object.

---
+++ src/Ai2Csproj/MigrationResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ai2Csproj
+{
+    internal class MigrationResult
+    {
+        // ---------------- Constructor ----------------
+
+        public MigrationResult( string assemblyInfoContents, string csprojContents )
+        {
+            this.AssemblyInfoContents = assemblyInfoContents;
+            this.CprojContents = csprojContents;
+        }
+
+        // ---------------- Properties ----------------
+
+        /// <summary>
+        /// The contents to write to the AssemblyInfo file.
+        /// If empty string, it means delete the file.
+        /// </summary>
+        public string AssemblyInfoContents { get; private set; }
+
+        /// <summary>
+        /// The contents of the csproj to write out.
+        /// </summary>
+        public string CprojContents { get; private set; }
+    }
+}
